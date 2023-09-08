edeb2e0e02203bab900622c4b954f59d240464b9
Zain Baquar <zain.baq@gmail.com>
2023-09-07 22:46:44 -0400

    update readme

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 # for mac install mssql driver
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-brew tap microsoft/mssql-release https://github.com/Microsoft/homebrew-mssql-release
-brew update
-HOMEBREW_ACCEPT_EULA=Y brew install msodbcsql18 mssql-tools18
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+# brew tap microsoft/mssql-release https://github.com/Microsoft/homebrew-mssql-release
+# brew update
+# HOMEBREW_ACCEPT_EULA=Y brew install msodbcsql18 mssql-tools18
 
-sudo ln -s /usr/local/etc/odbcinst.ini /etc/odbcinst.ini
-sudo ln -s /usr/local/etc/odbc.ini /etc/odbc.ini
+# sudo ln -s /usr/local/etc/odbcinst.ini /etc/odbcinst.ini
+# sudo ln -s /usr/local/etc/odbc.ini /etc/odbc.ini
+
+# Build virtual environment to run the application
+python -m venv querygpt
+source querygpt/bin/activate
+pip install -r requirements.txt
